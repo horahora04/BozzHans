32582cd48c45af1d19fc8d350fd46bd40fab631e
horahora04 <154544589+horahora04@users.noreply.github.com>
2025-03-04 09:15:15 +0700

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-MONGODB_URI = "mongodb://localhost:27017/"
-DB_NAME = "shopee_db"
-COLLECTION_NAME = "products"
-LICENSE_SERVER = "https://license-checker.horahora04.workers.dev"
-SECRET_KEY = "super_secret_key_123"
